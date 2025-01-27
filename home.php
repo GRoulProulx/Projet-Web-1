1e5be9c6744cba47e683570fd3d96ba930dd8cef
GRoulProulx <gproulx85@gmail.com>
2025-01-27 14:45:50 -0500

    Added Home Template

---
+++ home.php
@@ -0,0 +1,368 @@
+{{ include('layouts/header.php', {title:'Accueil'})}}
+
+<main>
+            <section class="hero-banner">
+                <img
+                    src="{{asset}}/img/hero-banner.webp"
+                    alt=""
+                    class="hero-image" />
+                <div class="hero-content">
+                    <h1>
+                        Bienvenue sur les enchères <br />
+                        du Lord Stampee!
+                    </h1>
+                    <p>Participez dès maintenant.</p>
+                    <button class="btn-banner">Voir les enchères</button>
+                </div>
+                <aside class="hero-aside">
+                    <div class="hero-members">
+                        <h2>Inscrirez-vous</h2>
+                        <p>
+                            Première fois sur le site? <br />Inscrirez-vous dès
+                            maintenant pour miser!
+                        </p>
+                        <button class="btn-member">S'inscrire</button>
+                    </div>
+                    <div class="hero-offers">
+                        <h2>Découvrez nos offres vedettes!</h2>
+                        <p></p>
+                        <a href="#">&#8618; Cliquez ici pour en savoir plus</a>
+                    </div>
+                </aside>
+            </section>
+            <div>
+                <h1 class="title">Les enchères rencentes</h1>
+            </div>
+            <section class="card-container">
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp1.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>FR #23 MNH OG XF</h3>
+                    <h4>3$</h4>
+                    <p>3h 40m | 2 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp2.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>UK #105 F-VF Used</h3>
+                    <h4>54$</h4>
+                    <p>9h 30m | 3 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp7.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>AUS #76 LH VF-XF</h3>
+                    <h4>18$</h4>
+                    <p>16h 30m Restant | 7 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp4.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>GER #456 CTO XF</h3>
+                    <h4>78$</h4>
+                    <p>7h 20m Restant | 5 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp5.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>IT #32 Used F-VF</h3>
+                    <h4>0.50$</h4>
+                    <p>9h 50m Restant | 11 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp6.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>CH #J28 NH VF</h3>
+                    <h4>78$</h4>
+                    <p>0h 30m Restant | 45 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp7.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>USA #261A VF/XF</h3>
+                    <h4>91$</h4>
+                    <p>1h 50m Restant | 33 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp8.webp"
+                                alt=""
+                        /></picture>
+                    </div>
+                    <h3>FR #53 MNH 8G XF</h3>
+                    <h4>8$</h4>
+                    <p>9h 30m Restant | 13 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp9.webp"
+                                alt=""
+                        /></picture>
+                    </div>
+                    <h3>FR #53 MNH 8G XF</h3>
+                    <h4>7$</h4>
+                    <p>14h 10m Restant | 33 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp10.webp"
+                                alt=""
+                        /></picture>
+                    </div>
+                    <h3>CAN #A23 MNH VF</h3>
+                    <h4>18$</h4>
+                    <p>4h 34m Restant | 21 mise</p>
+                </article>
+            </section>
+            <section class="small-banner">
+                <div>
+                    <h2>Tout sur les timbres</h2>
+                    <p>
+                        Notre marché est construit de A à Z pour comprendre les
+                        collectionneurs et leur façon de collectionner.
+                    </p>
+                </div>
+                <div>
+                    <h2>Vendeurs de confiance</h2>
+                    <p>
+                        La platforme Stampee abrite plus de 180 vendeurs de
+                        confiance, et chaque achat est couvert par la protection
+                        de PayPal.
+                    </p>
+                </div>
+                <div>
+                    <h2>Plus de 100 000 enchères</h2>
+                    <p>
+                        Parcourez une vaste collection de plus de cent milles de
+                        timbres du monde entier.
+                    </p>
+                </div>
+            </section>
+            <div>
+                <h1 class="title">Les enchères populaires</h1>
+            </div>
+            <section class="card-container">
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp5.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>IT #32 Used F-VF</h3>
+                    <h4>34.50$</h4>
+                    <p>3h 50m Restant | 11 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp11.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>ROM #J28 NH VF</h3>
+                    <h4>78$</h4>
+                    <p>34h 30m Restant | 45 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp7.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>CAN #J28 NH VF</h3>
+                    <h4>18$</h4>
+                    <p>14h 20m Restant | 25 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp7.webp"
+                                alt="timbres"
+                        /></picture>
+                    </div>
+                    <h3>USA #261A VF/XF</h3>
+                    <h4>91$</h4>
+                    <p>1h 50m Restant | 33 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp12.webp"
+                                alt=""
+                        /></picture>
+                    </div>
+                    <h3>US #53 MNH 8G XF</h3>
+                    <h4>18$</h4>
+                    <p>9h 30m Restant | 13 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp13.webp"
+                                alt=""
+                        /></picture>
+                    </div>
+                    <h3>US #53 MNH 8G XF</h3>
+                    <h4>7$</h4>
+                    <p>14h 10m Restant | 33 mise</p>
+                </article>
+                <article class="small-card">
+                    <div>
+                        <picture
+                            ><img
+                                class="small-img"
+                                src="{{asset}}/img/stamp10.webp"
+                                alt=""
+                        /></picture>
+                    </div>
+                    <h3>CAN #A23 MNH VF</h3>
+                    <h4>18$</h4>
+                    <p>4h 34m Restant | 21 mise</p>
+                </article>
+            </section>
+            <section class="small-banner center">
+                <div>
+                    <h2>
+                        LIVRAISON EMBALLAGE GRATUITS dans nos Ventes aux
+                        Enchères
+                    </h2>
+                    <h3>
+                        VOUS NE PAIEREZ PAS UN CENTIME POUR L'EXPÉDITION !
+                        <br />
+                        Tous les lots que vous remportez dans nos VENTES AUX
+                        ENCHÈRES HEBDOMADAIRES PAR <br />
+                        CORRESPONDANCE sont expédiés gratuitement dans le monde
+                        entier et aux Royaume-Uni.
+                    </h3>
+                </div>
+            </section>
+            <section class="big-card">
+                <div class="big-card-container">
+                    <div class="big-card-image">
+                        <picture
+                            ><img
+                                class="big-img"
+                                src="{{asset}}/img/louvre.webp"
+                                alt=""
+                        /></picture>
+                    </div>
+                    <div class="big-card-content">
+                        <h2>Actualités</h2>
+                        <h3>Un marché en pleine mutation</h3>
+                        <p>
+                            Cette vente aux enchères reflète les changements
+                            profonds que connaît actuellement le marché de la
+                            philatélie. Les collectionneurs semblent de plus en
+                            plus attirés par des pièces uniques ayant une
+                            histoire particulière, plutôt que par les
+                            "classiques" habituels. Ce phénomène ouvre de
+                            nouvelles perspectives pour les collectionneurs et
+                            les investisseurs, tout en posant de nouveaux défis
+                            aux experts du secteur. La soirée s'est conclue sur
+                            un chiffre d'affaires total de 2,7 millions d'euros,
+                            dépassant largement les estimations initiales. Ces
+                            résultats surprenants confirment que le monde de la
+                            philatélie reste dynamique et imprévisible, capable
+                            de passionner aussi bien les collectionneurs
+                            chevronnés que les nouveaux venus.
+                        </p>
+                        <button class="btn-news">Lire plus d'articles</button>
+                    </div>
+                </div>
+            </section>
+            <section class="news-letter">
+                <h3>
+                    Rester informé des enchères <br />
+                    actuallités, evenements de l'univers stampee!
+                </h3>
+                <p>
+                    Recever le meilleur de la maison stampee directement dans
+                    votre boîte de réception.
+                </p>
+                <input
+                    class="news-input"
+                    type="text"
+                    name="name"
+                    placeholder="Votre nom"
+                    aria-label="Votre nom" />
+                <input
+                    class="news-input"
+                    type="email"
+                    name="news-letter"
+                    placeholder="Adresse electronique"
+                    aria-label="Adresse electronique" />
+                <button>S'inscrire!</button>
+            </section>
+            <section class="social">
+                <img src="{{asset}}/img/facebook.webp" alt="" /><img
+                    src="{{asset}}/img/twitter.webp"
+                    alt="" /><img src="{{asset}}/img/instagram.webp" alt="" />
+            </section>
+        </main>
+
+{{ include('layouts/footer.php')}}
